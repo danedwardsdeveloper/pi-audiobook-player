eb1360c7ab6f7f2c708526a4b9a9db0018539356
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-22 09:59:35 +0000

    Formatting

--- testServer.ts
+++ testServer.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from 'express'
+import path from 'node:path'
+import express, { type Request, type Response } from 'express'
 import next from 'next'
-import path from 'path'
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -9,31 +9,33 @@ const handle = app.getRequestHandler()
 const port = 3000
 
 app.prepare().then(() => {
-  const server = express()
+	const server = express()
 
-  server.use('/_next', express.static(path.join(__dirname, '.next')))
+	server.use('/_next', express.static(path.join(__dirname, '.next')))
 
-  server.use('/_next/static/media', (req: Request, res: Response) => {
-    handle(req, res)
-  })
+	server.use('/_next/static/media', (req: Request, res: Response) => {
+		handle(req, res)
+	})
 
-  server.all('*', (req: Request, res: Response) => {
-    return handle(req, res)
-  })
+	server.all('*', (req: Request, res: Response) => {
+		return handle(req, res)
+	})
 
-  server.listen(port, (err?: Error) => {
-    if (err) throw err
-    // eslint-disable-next-line no-console
-    console.log(`> Ready on http://localhost:${port}`)
-  })
+	server.listen(port, (err?: Error) => {
+		if (err) throw err
+		// biome-ignore lint/suspicious/noConsole:
+		console.log(`> Ready on http://localhost:${port}`)
+	})
 })
 
 process.on('uncaughtException', (err: Error) => {
-  console.error('Uncaught Exception:', err)
-  process.exit(1)
+	// biome-ignore lint/suspicious/noConsole:
+	console.error('Uncaught Exception:', err)
+	process.exit(1)
 })
 
 process.on('unhandledRejection', (reason: unknown) => {
-  console.error('Unhandled Rejection:', reason)
-  process.exit(1)
+	// biome-ignore lint/suspicious/noConsole:
+	console.error('Unhandled Rejection:', reason)
+	process.exit(1)
 })
