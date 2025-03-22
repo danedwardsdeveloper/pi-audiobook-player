aa833e20c3c6c95c09874e08200ef5ebb30e5627
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-22 10:00:42 +0000

    Extract types

--- src/library/constants/definitions/temporaryAudiobooks.ts
+++ src/library/constants/definitions/temporaryAudiobooks.ts
@@ -1,29 +1,4 @@
-import type { StaticImageData } from 'next/image'
-
-import anneOfGreenGables from '@/images/anne.jpg'
-import dracula from '@/images/dracula.jpg'
-import janeEyre from '@/images/jane-eyre.jpg'
-
-export type TitleSlug = 'dracula' | 'anne-of-green-gables' | 'jane-eyre'
-
-interface Chapter {
-	name: string
-	duration: string
-	progress: number
-}
-
-export interface Audiobook {
-	titleDisplay: string
-	titleSlug: TitleSlug
-	writer: string
-	performer: string
-	totalSeconds: number
-	listenedSeconds: number
-	image: StaticImageData
-	chapters: Chapter[]
-}
-
-const chapters = [
+export const temporaryChapters = [
 	{ name: 'Intro', duration: '12:30', progress: 1 },
 	{ name: 'Chapter 1', duration: '24:45', progress: 1 },
 	{ name: 'Chapter 2', duration: '32:15', progress: 1 },
@@ -46,40 +21,3 @@ const chapters = [
 	{ name: 'Chapter 19', duration: '26:15', progress: 0 },
 	{ name: 'Chapter 20', duration: '42:10', progress: 0 },
 ]
-
-export const temporaryAudiobooks: Audiobook[] = [
-	{
-		titleDisplay: 'Anne of Green Gables',
-		titleSlug: 'anne-of-green-gables',
-		writer: 'Lucy Maud Montgomery',
-		performer: 'Rachael McAdams',
-		totalSeconds: 28800,
-		listenedSeconds: 19387,
-		image: anneOfGreenGables,
-		chapters,
-	},
-	{
-		titleDisplay: 'Dracula',
-		titleSlug: 'dracula',
-		writer: 'Bram Stoker',
-		performer: 'Alan Cumming',
-		totalSeconds: 32400,
-		listenedSeconds: 12960,
-		image: dracula,
-		chapters,
-	},
-	{
-		titleDisplay: 'Jane Eyre',
-		titleSlug: 'jane-eyre',
-		writer: 'Charlotte Brontë',
-		performer: 'Thandiwe Newton',
-		totalSeconds: 36000,
-		listenedSeconds: 5400,
-		image: janeEyre,
-		chapters,
-	},
-]
-
-export function getAudiobookBySlug(slug: TitleSlug): Audiobook | undefined {
-	return temporaryAudiobooks.find((audiobook) => audiobook.titleSlug === slug)
-}
