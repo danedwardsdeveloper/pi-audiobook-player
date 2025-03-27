2d61d6e32a28dad1b523c38bfe293ce5dc3c4102
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-27 21:28:45 +0000

    Create tables

--- src/library/database/schema.ts
+++ src/library/database/schema.ts
@@ -1,31 +1,28 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
-export const tracks = sqliteTable('tracks', {
+export const audiobooks = sqliteTable('audiobooks', {
 	id: integer('id').primaryKey({ autoIncrement: true }),
 	name: text('name').notNull().unique(),
 })
 
-export type TrackInsertValues = typeof tracks.$inferInsert
-export type Track = typeof tracks.$inferSelect
+export type AudiobookInsertValues = typeof audiobooks.$inferInsert
+export type AudiobookRow = typeof audiobooks.$inferSelect
 
-export const playHistory = sqliteTable('play_history', {
+export const tracks = sqliteTable('tracks', {
 	id: integer('id').primaryKey({ autoIncrement: true }),
-	trackId: integer('trackId').references(() => tracks.id),
+	audiobookId: integer('audiobook_id')
+		.references(() => audiobooks.id)
+		.notNull(),
+	name: text('name').notNull(),
+	durationSeconds: integer('duration_seconds').notNull(),
 })
 
-/*
+export type TrackInsertValues = typeof tracks.$inferInsert
+export type TrackRecord = typeof tracks.$inferSelect
 
-
-tracks
-
-
-play_history
-
-
-
-
-
-
-
-
-*/
+export const progress = sqliteTable('progress', {
+	trackId: integer('track_id')
+		.references(() => tracks.id)
+		.primaryKey(),
+	positionSeconds: integer('position_seconds'),
+})
