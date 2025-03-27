00dedf2363aeb54fb5bede7017c8cfc286ef8375
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-27 21:30:57 +0000

    Remove comments

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,5 @@
-// import aspectRatio from '@tailwindcss/aspect-ratio'
-import { type Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss'
 
 export default {
-  plugins: [
-    // aspectRatio
-  ],
-  content: ['./src/**/*.{ts,tsx}'],
-  darkMode: 'selector',
-  theme: {
-    extend: {
-      fontFamily: {
-        // sans: ['var(--font-readex-pro)'],
-      },
-    },
-  },
+	content: ['./src/**/*.{ts,tsx}'],
 } satisfies Config
