81793e636812a2936d6653998f09c0774ea548fd
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-27 21:30:41 +0000

    Add database handling

--- src/library/utilities/server/definitions/getFolders.ts
+++ src/library/utilities/server/definitions/getFolders.ts
@@ -1,6 +1,9 @@
 import { readdir, stat } from 'node:fs/promises'
 import path from 'node:path'
+import database from '@/library/database/connection'
+import { audiobooks } from '@/library/database/schema'
 import logger from '@/library/logger'
+import { inArray } from 'drizzle-orm'
 
 export const runtime = 'nodejs'
 
@@ -22,6 +25,18 @@ export async function getFolders(): Promise<string[]> {
 		const results = await Promise.all(folderPromises)
 
 		const folders = results.filter((folder): folder is string => folder !== null).sort((a, b) => a.localeCompare(b))
+
+		const existingAudiobooks = await database.select({ name: audiobooks.name }).from(audiobooks).where(inArray(audiobooks.name, folders))
+
+		const existingNames = existingAudiobooks.map((book) => book.name)
+
+		const newFolders = folders.filter((folder) => !existingNames.includes(folder))
+
+		if (newFolders.length > 0) {
+			const valuesToInsert = newFolders.map((name) => ({ name }))
+
+			await database.insert(audiobooks).values(valuesToInsert)
+		}
 
 		return folders
 	} catch (error) {
