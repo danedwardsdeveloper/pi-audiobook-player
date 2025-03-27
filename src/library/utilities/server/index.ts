ab227f3c73d0ba98cbac5fbdc5acf0ff9b42abf7
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-27 21:30:51 +0000

    Rename getTracks

--- src/library/utilities/server/index.ts
+++ src/library/utilities/server/index.ts
@@ -1,4 +1,4 @@
 export * from './definitions/getPhotos'
-export * from './definitions/getFiles'
+export * from './definitions/getTracks'
 export * from './definitions/getFolders'
 export * from './definitions/getMetadata'
