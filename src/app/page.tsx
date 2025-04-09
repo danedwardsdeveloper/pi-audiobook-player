097e28e281a570375e054f30e345f4846518ed3e
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-09 09:14:44 +0100

    Remove BookShelf component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-import BookShelf from '@/components/BookShelf'
+'use client'
+import BookCard from '@/components/BookCard'
+import { useAudiobooks } from '@/providers/audiobook'
 
-export default function Page() {
-	return <BookShelf />
+export default function HomePage() {
+	const { audiobooks, isLoadingAudiobooks } = useAudiobooks()
+
+	if (isLoadingAudiobooks) return null
+	if (!audiobooks) return <div>No audiobooks found</div>
+
+	return (
+		<div className="max-w-full w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-8 gap-6">
+			{audiobooks.map((audiobook) => (
+				<BookCard key={audiobook.titleSlug} audiobook={audiobook} />
+			))}
+		</div>
+	)
 }
